cae4dd9ea8ee6addc465f27eaeffe7731985ed12
Sam <sammyryan93@hotmail.co.uk>
2018-07-04 20:43:17 +0100

    Initial commit

---
+++ app/src/main/java/com/mobdev/sam/apprenticeapp/fragments/HomeFragment.java
@@ -0,0 +1,25 @@
+package com.mobdev.sam.apprenticeapp.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mobdev.sam.apprenticeapp.R;
+
+/**
+ * Created by Sam on 02/07/2018.
+ */
+
+public class HomeFragment extends android.support.v4.app.Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.home_layout, container, false);
+        return myView;
+    }
+}
